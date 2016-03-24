559932900a0fa9459f816570cce6f70b19e13c02
Catherine Karra and kate hall <catherine@>
2016-03-24 15:50:11 -0700

    add if else if statements in js to select triangle types

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,25 @@
 $(document).ready(function() {
+  $("#triangle").submit(function(event) {
+    var S1userinput= parseInt($("input#side1").val());
+    var S2userinput= parseInt($("input#side2").val());
+    var S3userinput= parseInt($("input#side3").val());
 
+
+    if (S1userinput, S2userinput === S3userinput) {
+      var equilateral = "equilateral";
+      $(".triangle").text(equilateral);
+      $("#results").show();
+    } else if (S1userinput === S2userinput || S1userinput === S3userinput || S2userinput === S3userinput) {
+      var isosceles= "isosceles";
+      $(".triangle").text(isosceles);
+      $("#results").show();
+  } else if (S1userinput !== S2userinput && S2userinput !== S3userinput && S1userinput !== S3userinput) {
+      var scalene= "scalene";
+      $(".triangle").text(scalene);
+      $("#results").show();
+    } else {
+      alert("Doesn't fit neatly into our triangle categories, sowy.");
+    }
+    event.preventDefault();
+  });
 });
